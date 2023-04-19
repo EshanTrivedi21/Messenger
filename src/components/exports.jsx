69960b19b0ecd8041f1852593aa27a4fb61d48a8
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-04-19 22:04:12 +0530

    feat: components setup

---
+++ src/components/exports.jsx
@@ -0,0 +1,17 @@
+import Chat from './Chat';
+import Chats from './Chats';
+import Input from './Input';
+import Message from './Message';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+import Searchbar from './Searchbar';
+
+export {
+    Chat,
+    Chats,
+    Input,
+    Message,
+    Navbar,
+    Sidebar,
+    Searchbar
+}
